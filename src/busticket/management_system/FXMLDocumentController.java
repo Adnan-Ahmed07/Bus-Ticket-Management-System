abf2da8a10678df4517cd1b67862d5db9751221a
Adnan-Ahmed07 <masudrana52443@gmail.com>
2024-12-28 14:01:27 -0700

    Landing and login page working

---
+++ src/busticket/management_system/FXMLDocumentController.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package busticket.management_system;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author User
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    private Label label;
+    @FXML
+    private PasswordField passwordin;
+    @FXML
+    private Button login_in;
+    @FXML
+    private Button signupin;
+    @FXML
+    private TextField usernamenew;
+    
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
