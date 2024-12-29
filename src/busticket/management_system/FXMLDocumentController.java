43056c838b6d028b3be7552b81281ad9a49f47fb
Adnan-Ahmed07 <masudrana52443@gmail.com>
2024-12-29 14:33:15 -0700

    my login side password and username taking database perfectly

--- src/busticket/management_system/FXMLDocumentController.java
+++ src/busticket/management_system/FXMLDocumentController.java
@@ -1,74 +1,152 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package busticket.management_system;
 
-import com.mysql.jdbc.Connection;
-import java.sql.ResultSet;
-import com.mysql.jdbc.PreparedStatement;
-import java.net.URL;
-import java.util.ResourceBundle;
 
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
- * @author User Adnan Ahmed
+ * @author Adnan Ahmed 
+ 
  */
 public class FXMLDocumentController implements Initializable {
-    
-  @FXML
-    private Button login_in;
 
     @FXML
     private AnchorPane main_form;
 
     @FXML
-    private PasswordField passwordin;
+    private TextField username;
 
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private Button login;
+
+    @FXML
+    private Button close;
+
+
+    private Connection connect;
+    private PreparedStatement prepare;
+    private ResultSet result;
+
+    private double x = 0;
+    private double y = 0;
+    @FXML
+    private ImageView usernamenew;
     @FXML
     private Button signupin;
 
     @FXML
-    private TextField usernamenew;
-     @FXML
-    private Button close;
-     
-    private Connection connect;
-    private PreparedStatement prepare;
-    private ResultSet result; 
-    public void login_in(){ 
+    public void login() {
+
         String sql = "SELECT * FROM admin WHERE username = ? and password = ?";
 
         connect = database.connectDb();
 
         Alert alert;
-     
+
+        try {
+
+            if (username.getText().isEmpty() || password.getText().isEmpty()) {
+
+                alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Ërror Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please fill all blank fields");
+                alert.showAndWait();
+
+            } else {
+
+                prepare = connect.prepareStatement(sql);
+                prepare.setString(1, username.getText());
+                prepare.setString(2, password.getText());
+
+                result = prepare.executeQuery();
+
+                if (result.next()) {
+
+                    getData.username = username.getText();
+
+                    // THEN WE WILL PROCEED TO DASHBOARD FORM : ) 
+                    alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Login!");
+                    alert.showAndWait();
+                    login.getScene().getWindow().hide();
+
+                    Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(root);
+
+                    root.setOnMousePressed((MouseEvent event) -> {
+                        x = event.getSceneX();
+                        y = event.getSceneY();
+                    });
+
+                    root.setOnMouseDragged((MouseEvent event) -> {
+                        stage.setX(event.getScreenX() - x);
+                        stage.setY(event.getScreenY() - y);
+                    });
+
+                    stage.initStyle(StageStyle.TRANSPARENT);
+
+                    stage.setScene(scene);
+                    stage.show();
+
+                } else {
+//                    IF INCORRECT THE DATA YOU GAVE 
+                    alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Wrong Username/Password");
+                    alert.showAndWait();
+
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
-    try{ 
- 
-}
-    
-    
-     
-  @FXML
-     public void close(){ 
-         
-         System.exit(0);
-     }
-    
-   
-    
+
+    @FXML
+    public void close() {
+        System.exit(0);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
---
+++ src/busticket/management_system/getData.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package busticket.management_system;
+
+/**
+ *
+ * @author Adnan Ahmed
+  
+ */
+public class getData {
+    
+    public static Integer number;
+    public static String username;
+    
+}
