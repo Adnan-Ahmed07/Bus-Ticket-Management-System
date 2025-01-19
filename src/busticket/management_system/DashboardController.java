2e0e3bfeab8b6de06746a517f555506a8097cdde
Adnan-Ahmed07 <masudrana52443@gmail.com>
2025-01-19 10:51:11 -0700

    Logout syestem added working on busdata

--- src/busticket/management_system/DashboardController.java
+++ src/busticket/management_system/DashboardController.java
@@ -4,22 +4,37 @@
  */
 package busticket.management_system;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
+import java.sql.ResultSet;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.AreaChart;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -235,31 +250,125 @@ public class DashboardController implements Initializable {
     @FXML
     private Label username;
     
+     private Connection connect;
+    private PreparedStatement prepare;
+    private ResultSet result;
+    private Statement statement;
     
+    
+    public ObservableList<busData> availableBusBusData() {
+
+        ObservableList<busData> busListData = FXCollections.observableArrayList();
+
+        String sql = "SELECT * FROM bus";
+
+        connect = database.connectDb();
+
+        try {
+
+            prepare = connect.prepareStatement(sql);
+            result = prepare.executeQuery();
+
+            busData busD;
+
+            while (result.next()) {
+                busD = new busData(result.getInt("bus_id"),
+                        result.getString("location"),
+                        result.getString("status"),
+                        result.getDouble("price"),
+                        result.getDate("date"));
+
+                busListData.add(busD);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return busListData;
+
+    }
+    
+    
+    
+    private double x = 0;
+    private double y = 0;
+    public void logout() {
+
+        try {
+
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Are you sure you want to logout?");
+
+            Optional<ButtonType> option = alert.showAndWait();
+
+            if (option.get().equals(ButtonType.OK)) {
+
+                logout.getScene().getWindow().hide();
+
+                Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+                Stage stage = new Stage();
+                Scene scene = new Scene(root);
+
+                root.setOnMousePressed((MouseEvent event) -> {
+                    x = event.getSceneX();
+                    y = event.getSceneY();
+                });
+
+                root.setOnMouseDragged((MouseEvent event) -> {
+
+                    stage.setX(event.getScreenX() - x);
+                    stage.setY(event.getScreenY() - y);
+
+                    stage.setOpacity(.8);
+                });
+                root.setOnMouseReleased((MouseEvent event) -> {
+                    stage.setOpacity(1);
+                });
+
+                stage.initStyle(StageStyle.TRANSPARENT);
+
+                stage.setScene(scene);
+                stage.show();
+
+            } else {
+                return;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     public void switchForm(ActionEvent event){ 
       if (event.getSource() == dashboard_Btn) {
            dashboard_form.setVisible(true);
             availableB_form.setVisible(false);
             bookingTicket_form.setVisible(false);
             customer_Form.setVisible(false);
+             
             
         }else if (event.getSource() == availableB_Btn){ 
             dashboard_form.setVisible(false);
             availableB_form.setVisible(true);
             bookingTicket_form.setVisible(false);
             customer_Form.setVisible(false);
+             
          
         }else if (event.getSource() == bookingTicket_Btn){ 
             dashboard_form.setVisible(false);
             availableB_form.setVisible(false);
             bookingTicket_form.setVisible(true);
             customer_Form.setVisible(false);
+                    
          
         }else if (event.getSource() == customers_Btn){ 
             dashboard_form.setVisible(false);
             availableB_form.setVisible(false);
             bookingTicket_form.setVisible(false);
             customer_Form.setVisible(true);
+                       
         
         }
      
